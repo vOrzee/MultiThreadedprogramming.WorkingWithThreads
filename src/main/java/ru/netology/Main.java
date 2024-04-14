12dc12008968ebade1448b9a06f6eecb008480d2
Roman Leshin <vorzee@gmail.com>
2024-04-14 11:50:14 +0500

    Task completed

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -11,27 +11,39 @@ public class Main {
         }
 
         long startTs = System.currentTimeMillis(); // start time
+
+        List<Thread> threads = new ArrayList<>();
+
         for (String text : texts) {
-            int maxSize = 0;
-            for (int i = 0; i < text.length(); i++) {
-                for (int j = 0; j < text.length(); j++) {
-                    if (i >= j) {
-                        continue;
-                    }
-                    boolean bFound = false;
-                    for (int k = i; k < j; k++) {
-                        if (text.charAt(k) == 'b') {
-                            bFound = true;
-                            break;
+            Thread thread = new Thread(() -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
                         }
                     }
-                    if (!bFound && maxSize < j - i) {
-                        maxSize = j - i;
-                    }
                 }
-            }
-            System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+            });
+            threads.add(thread);
+            thread.start();
         }
+
+        for (Thread thread : threads) {
+            thread.join(); // зависаем, ждём когда поток объект которого лежит в thread завершится
+        }
+
         long endTs = System.currentTimeMillis(); // end time
 
         System.out.println("Time: " + (endTs - startTs) + "ms");
